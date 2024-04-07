21b57390716374b910ae3f2abef3109afeb92a9c
guirosasantos <guirosasantos1010@gmail.com>
2024-04-07 17:33:57 -0300

    Adicionando comentários para auxiliar na apresentação

--- Apresentação no Display/apresentacao.c
+++ Apresentação no Display/apresentacao.c
@@ -3,31 +3,32 @@
 #include <fcntl.h>
 
 int main() {
-    int file;
-    char * pipefile = "/tmp/pipefile";
+    int file;                                                   // Descritor do arquivo
+    char * pipefile = "/tmp/pipefile";                          // Seleciona o arquivo que será usado para a comunicação entre os processos
     int bytesRead;
 
     int contagem;
     float pesoTotal;
     float ultimoPeso = 0;
 
-    file = open(pipefile, O_RDONLY);
+    file = open(pipefile, O_RDONLY);                            // Abre o arquivo para leitura
 
     while(1) {
-        bytesRead = read(file, &contagem, sizeof(contagem));
+        bytesRead = read(file, &contagem, sizeof(contagem));    // Lê o arquivo e armazena o valor na variável contagem
         if(bytesRead != 0) {
-            printf("Contagem: %d\n", contagem);
+            printf("Contagem: %d\n", contagem);                 // Exibe o valor da contagem
         }
 
-        bytesRead = read(file, &pesoTotal, sizeof(pesoTotal));
+        bytesRead = read(file, &pesoTotal, sizeof(pesoTotal));  // Lê o arquivo e armazena o valor na variável pesoTotal
         if(bytesRead != 0) {
-            printf("Peso total: %.1fkg\n", pesoTotal);
+            printf("Peso total: %.1fkg\n", pesoTotal);          // Exibe o valor do peso total
         }
 
         sleep(1);
     }
 
-    close(file);
+    close(file);                                                // Fecha o descritor do arquivo
+    unlink(pipefile)                                            // Deleta o pipe
 
     return 0;
 }
--- Contagem/contagem.c
+++ Contagem/contagem.c
@@ -6,38 +6,38 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-float pesos[1500];
+float pesos[1500];                                              // Vetor que armazena os pesos  
 int contagem = 0;
 int contagemTotal = 0;
 int running = 1;
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;              // Inicializa o mutex
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;                 // Inicializa a variável de condição
 
 typedef struct {
     float peso;
     int sleepTime;
 } EsteiraInfo;
 
-void* EsteiraThread(void* arg) {
+void* EsteiraThread(void* arg) {                                // Thread que simula as esteiras
     EsteiraInfo* info = (EsteiraInfo*) arg;
     while (1){
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&mutex);                             // Locka as variáveis compartilhadas com o mutex para evitar condição de corrida
         while (!running) {
-            pthread_cond_wait(&cond, &mutex);
+            pthread_cond_wait(&cond, &mutex);                   // Coloca a thread em espera até que a variável de condição seja satisfeita e libera o mutex
         }
         if (contagem < 1500){
             pesos[contagem] = info->peso;
             contagem++;
             contagemTotal++;
         }
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex);                           // Desbloqueia o mutex
         usleep(info->sleepTime);
     }
     return NULL;
 }
 
-void* InputThread(void* arg) {
+void* InputThread(void* arg) {                                  // Thread que lê a entrada do usuário
     char input[64];
     while (1) {
         fgets(input, 64, stdin);
@@ -46,7 +46,7 @@ void* InputThread(void* arg) {
             running = 0;
         } else if (strcmp(input, "resume\n") == 0) {
             running = 1;
-            pthread_cond_broadcast(&cond);
+            pthread_cond_broadcast(&cond);                      // Acorda todas as threads que estão esperando na variável de condição
         }
         pthread_mutex_unlock(&mutex);
     }
@@ -63,7 +63,7 @@ void atualizarPeso(float *pesoAtual) {
     }
 }
 
-void atualizarContagem() {
+void resetarContagem() {
     contagem = 0;
 }
 
@@ -72,47 +72,48 @@ void retornarThreads() {
 }
 
 int main() {
-    int file;
-    char * pipefile = "/tmp/pipefile";
-    pthread_t esteira1_thread_id, esteira2_thread_id, esteira3_thread_id, input_thread_id;
+    int file;                                                   // Descritor do arquivo
+    char * pipefile = "/tmp/pipefile";                          // Seleciona o arquivo que será usado para a comunicação entre os processos
+    pthread_t esteira1_thread_id, esteira2_thread_id, esteira3_thread_id, input_thread_id;  // Identificador das threads
     float pesoAtual = 0;
 
-    EsteiraInfo esteira1Info = {5, 1000000};
-    EsteiraInfo esteira2Info = {2, 500000};
-    EsteiraInfo esteira3Info = {0.5, 100000};
+    EsteiraInfo esteira1Info = {5, 1000000};                    // Define as informações da esteira 1
+    EsteiraInfo esteira2Info = {2, 500000};                     // Define as informações da esteira 2
+    EsteiraInfo esteira3Info = {0.5, 100000};                   // Define as informações da esteira 3
 
-    pthread_create(&esteira1_thread_id, NULL, EsteiraThread, &esteira1Info);
-    pthread_create(&esteira2_thread_id, NULL, EsteiraThread, &esteira2Info);
-    pthread_create(&esteira3_thread_id, NULL, EsteiraThread, &esteira3Info);
+    pthread_create(&esteira1_thread_id, NULL, EsteiraThread, &esteira1Info); // Cria a thread da esteira 1
+    pthread_create(&esteira2_thread_id, NULL, EsteiraThread, &esteira2Info); // Cria a thread da esteira 2
+    pthread_create(&esteira3_thread_id, NULL, EsteiraThread, &esteira3Info); // Cria a thread da esteira 3
 
-    pthread_create(&input_thread_id, NULL, InputThread, NULL);
+    pthread_create(&input_thread_id, NULL, InputThread, NULL); // Cria a thread de input
 
-    mkfifo(pipefile, 0666);
-    file = open(pipefile, O_WRONLY);
+    mkfifo(pipefile, 0666);                                     // Cria um pipe com permissões de leitura e escrita no arquivo pipefile
+    file = open(pipefile, O_WRONLY);                            // Abre o arquivo para escrita
 
     while (1) {
         pthread_mutex_lock(&mutex);
         if (running) {
-            write(file, &contagemTotal, sizeof(contagemTotal));
+            write(file, &contagemTotal, sizeof(contagemTotal)); // Escreve a contagem no arquivo
 
-            if (contagem >= 1500){
+            if (contagem >= 1500){                              // Se a contagem for maior ou igual a 1500, atualiza o peso e reseta a contagem
                 pararThreads();
                 atualizarPeso(&pesoAtual);
-                atualizarContagem();
+                resetarContagem();
                 retornarThreads();
             }
-            write(file, &pesoAtual, sizeof(pesoAtual));
+            write(file, &pesoAtual, sizeof(pesoAtual));         // Escreve o peso total no arquivo 
         }
         pthread_mutex_unlock(&mutex);
         sleep(1);
     }
 
-    pthread_join(esteira1_thread_id, NULL);
-    pthread_join(esteira2_thread_id, NULL);
-    pthread_join(esteira3_thread_id, NULL);
+    pthread_cancel(esteira1_thread_id);                         // Cancela a thread da esteira 1
+    pthread_cancel(esteira2_thread_id);                         // Cancela a thread da esteira 2
+    pthread_cancel(esteira3_thread_id);                         // Cancela a thread da esteira 3
+    pthread_cancel(input_thread_id);                            // Cancela a thread de input
 
-    close(file);
-    unlink(pipefile);
+    close(file);                                                // Fecha o descritor do arquivo
+    unlink(pipefile);                                           // Deleta o pipe
 
     return 0;
 }
